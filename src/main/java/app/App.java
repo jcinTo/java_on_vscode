c2cae981b4330320e9c7e0d40621d4614c163c89
jcabannes <jcabannes@linagora.com>
2018-02-16 17:36:31 +0100

    Created a class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,8 +1,79 @@
 package app;
 
+import java.util.Scanner;
+
 public class App{
     
+    public static double arrondi(double A, int B) {
+        return (double) ( (int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello world");
+
+        // Lire une entrée utilisateur
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Saisi ton nom");
+        String str = sc.nextLine();
+        System.out.println("Voilà ce que tu as saisi : " + str);
+        sc.close();*/
+        
+        /**
+         * Cas particulier où nextLine réinitialise la ligne de saisie et ne nous permet pas de faire de saisie 
+         */
+        /* Scanner sc = new Scanner(System.in);
+        System.out.println("Saisissez un entier : ");
+        int i = sc.nextInt();
+        System.out.println("Saisissez une chaîne : ");
+
+        //On vide la ligne avant d'en lire une autre
+        sc.nextLine(); // Hack permettant de corriger la nom prise en charge de saisie par nextLine
+
+        String str = sc.nextLine();      
+        System.out.println("FIN ! " + str);
+        sc.close();
+        */
+
+        /**
+         * TP conversion Celsius/Fahrenheit
+         */
+        /*System.out.println("CONVERTISSEUR DEGRES CELIUS ET DEGRES FARHRENHEIT");
+        System.out.println("-------------------------------------------------");
+        
+        char reponse = ' ';
+        Scanner sc = new Scanner(System.in);
+        double result;
+        do{
+            System.out.println("Choisissez le mode de conversion :");
+            System.out.println("1 - Convertisseur Celsius - Fahrenheit");
+            System.out.println("2 - Convertisseur Fahrenheit - Celsius");
+            int mode = sc.nextInt();
+            System.out.println("Température à convertir : ");
+            sc.nextLine();
+            double temp = sc.nextDouble();
+            switch(mode){
+                case 1: 
+                    result = arrondi(((9.0/5.0)*temp+32), 1);
+                    System.out.println(arrondi(temp, 1) + " °C correspond à : " + result + " °F.");
+                    break;
+                case 2:
+                    result = arrondi((((temp-32)*5.0)/9.0), 1);
+                    System.out.println(arrondi(temp, 1) + " °F correspond à : " + result + " °C.");
+                    break;
+                default:
+                    System.out.println("C'est pas sérieux");
+            }
+            do{
+                System.out.println("Souhaitez-vous convertir une autre température? (O/N)");
+                sc.nextLine();
+                reponse = sc.nextLine().charAt(0);
+            } while (reponse != 'O' && reponse != 'N');
+        } while(reponse == 'O');
+        System.out.println("Au revoir !");
+        sc.close();*/
+
+        /**
+         * Création d'une ville
+         */
+        Ville ville = new Ville();
     }
 }
---
+++ src/main/java/app/Ville.java
@@ -0,0 +1,122 @@
+package app;
+
+public class Ville{
+
+    // On les met en private pour pas quelles soient accessible via une instance de la classe Ville, exemple ville.nom
+    // Ainsi on se protège de toutes modifications des valeurs depuis une autre classe 
+    private String nom;
+    private String pays;
+    private int nbHabitants;
+    private char categorie;
+
+    //Variables publiques qui comptent les instances
+    public static int nbreInstances = 0;
+
+    //Variable privée qui comptera aussi les instances
+    private static int nbreInstancesBis = 0;       
+
+    public Ville(){
+        System.out.println("Création d'une ville!");
+        nom = "Marciac";
+        pays = "Gascogne";
+        nbHabitants = 10;
+        this.setCategorie();
+
+        //On incrémente nos variables à chaque appel aux constructeurs
+        nbreInstances++;
+        nbreInstancesBis++;  
+    }
+
+    public Ville(String pNom, String pPays, int pNbHabitants){
+        System.out.println("Création d'une ville!");
+        nom = pNom;
+        pays = pPays;
+        nbHabitants = pNbHabitants;
+        this.setCategorie();
+
+        //On incrémente nos variables à chaque appel aux constructeurs
+        nbreInstances++;
+        nbreInstancesBis++;  
+    }
+
+    //*************   ACCESSEURS *************
+    
+    //Retourne le nom de la ville
+    public String getNom()  {  
+        return nom;
+    }
+
+    //Retourne le nom du pays
+    public String getNomPays()
+    {
+        return pays;
+    }
+
+    // Retourne le nombre d'habitants
+    public int getNombreHabitants()
+    {
+        return nbHabitants;
+    } 
+
+    //Retourne la catégorie de la ville
+    public char getCategorie()
+    {
+        return categorie;
+    }
+
+    public static int getNombreInstancesBis()
+    {
+        return nbreInstancesBis;
+    }  
+    
+    //*************   MUTATEURS   *************
+
+    //Définit le nom de la ville
+    public void setNom(String pNom)
+    {
+        nom = pNom;
+    }
+
+    //Définit le nom du pays
+    public void setNomPays(String pPays)
+    {
+        pays = pPays;
+    }
+
+    //Définit le nombre d'habitants
+    public void setNombreHabitants(int nbre)
+    {
+        nbHabitants = nbre;
+    }
+    
+    //Définit la catégorie de la ville
+    private void setCategorie() {
+    
+        int bornesSuperieures[] = {0, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000};
+        char categories[] = {'?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+        int i = 0;
+        while (i < bornesSuperieures.length && this.nbHabitants > bornesSuperieures[i])
+        i++;
+
+        this.categorie = categories[i];
+    }
+
+    //Retourne la description de la ville
+    public String decrisToi(){
+        return "\t"+this.nom+" est une ville de "+this.pays+ ", elle comporte : "+this.nbHabitants+" habitant(s) => elle est donc de catégorie : "+this.categorie;
+    }
+
+    //Retourne une chaîne de caractères selon le résultat de la comparaison
+    public String comparer(Ville v1){
+        String str = new String();
+
+        if (v1.getNombreHabitants() > this.nbHabitants)
+        str = v1.getNom()+" est une ville plus peuplée que "+this.nom;
+        
+        else
+        str = this.nom+" est une ville plus peuplée que "+v1.getNom();
+        
+        return str;
+    }
+}
